282f60c09215ae579a5e90a9a201bf64f34049e7
dshaw0004 <114005178+dshaw0004@users.noreply.github.com>
2023-12-24 21:21:08 +0530

    first commit

--- fstore.py
+++ fstore.py
@@ -1,4 +1,5 @@
 from random import randint
+from datetime import datetime
 
 from firebase_admin import firestore
 
@@ -10,17 +11,17 @@ db = firestore.client(app)
 def get_all_message():
     ref = db.collection("messages")
     docs = ref.stream()
-    all_messages = []
+    all_todo = []
     for doc in docs:
         #        print(f"{doc.id} => {doc.to_dict()}")
-        all_messages.append(doc.to_dict())
+        all_todo.append(doc.to_dict())
 #    print(all_messages)
-    return all_messages
+    return all_todo
 
 
-def add_new_message(message: str, senderName: str, senderContact: str):
+def add_new_message(title: str, desc: str, extra: object = {}):
 
     no_of_message = randint(1, 99999) * randint(5, 77635)
     doc_ref = db.collection("messages").document(f"devp{no_of_message}")
-    doc_ref.set({"message": message, "senderName": senderName,
-                "senderContact": senderContact})
+    doc_ref.set({"title": title, "description": desc, "isCompleted": False, "creationDate": datetime.now(),
+                "extraStuff": extra})
--- main.py
+++ main.py
@@ -5,14 +5,7 @@ from flask_cors import CORS, cross_origin
 
 from fstore import add_new_message, get_all_message
 
-ALLOWED_ORIGIN = [
-    'https://dshaw0004.netlify.app',
-    'https://dshaw0004.web.app',
-    'https://dshaw0004.firebaseapp.com',
-    'https://dshaw0004.vercel.app',
-    # 'http://192.168.199.249:6970/index.html',
-    'http://localhost:3000'
-]
+
 
 app = Flask(__name__)
 CORS(app, resources={
@@ -47,8 +40,8 @@ def index():
     }
 
 
-@app.route("/getallmsg")
-def allmes():
+@app.route("/getalltodo")
+def getalltodo():
     auth = request.headers.get('Authorization')
     if auth == os.getenv('AUTH'):
         allmess = get_all_message()
@@ -56,41 +49,14 @@ def allmes():
     else:
         return jsonify({"permission": "denied"}), 404
 
-@app.route("/addnewmsg", methods=['POST'])
-def addnewmsg():
+@app.route("/addnewtodo", methods=['POST'])
+def addnewtodo():
     data = request.json
-    add_new_message(data.get('message'), data.get('senderName'), data.get('senderContact'))
+    add_new_message(data.get('title'), data.get('description'))
     return {
-        'your_name': data.get('senderName'),
-        'your_message': data.get('message'),
-        'your_contact': data.get('senderContact')
-    }
+        'title': data.get('title'),
+        'description': data.get('description'),
 
-
-@app.route("/login", methods=['POST'])
-def login():
-    data = request.json
-    if data.get('email') == os.getenv("email") and data.get("password") == os.getenv("password"):
-        return {
-            'access': 'granted',
-            'id': 'admin'
-        }
-    else:
-        return {
-            'access': 'denied',
-            'id': 'unknown'
-        }
-
-@app.route("/appstore", methods=['post'])
-def addnewmsg_appstore():
-    
-    data = request.json
-    add_new_message(message="for appstore"+data.get('message'), senderName=data.get(
-        'senderName'), senderContact=data.get('senderContact'))
-    return {
-        'your_name': data.get('senderName'),
-        'your_message': data.get('message'),
-        'your_contact': data.get('senderContact')
     }
 
 
